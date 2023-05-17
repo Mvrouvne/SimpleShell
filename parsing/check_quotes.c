1aaf5953485e95696b83fbdb86b80a45607788ff
Mvrouvne <77933958+Mvrouvne@users.noreply.github.com>
2023-05-17 23:21:58 +0100

    fixing quotes

--- parsing/check_quotes.c
+++ parsing/check_quotes.c
@@ -6,7 +6,7 @@
 /*   By: machaiba <machaiba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 12:16:31 by machaiba          #+#    #+#             */
-/*   Updated: 2023/05/17 23:19:10 by machaiba         ###   ########.fr       */
+/*   Updated: 2023/05/17 23:21:12 by machaiba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,13 @@
 int	check_quotes(t_token **lst, char *line, int *x)
 {
 	char *str;
+	int	d_quote;
+	int	s_quote;
 
 	str = malloc (sizeof(char));
 	str[0] = '\0';
-	int	d_quote = 0;
-	int	s_quote = 0;
+	d_quote = 0;
+	s_quote = 0;
 	while (line[*x])
 	{
 		if (line[*x] == '"')
