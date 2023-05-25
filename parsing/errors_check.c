94e93c82463328a9a3043c047d8083f79339b5c6
Marouane Chaibat <machaiba@Mr1s-Macbook-Pro.local>
2023-05-25 23:27:02 +0100

    some changes

--- parsing/errors_check.c
+++ parsing/errors_check.c
@@ -6,7 +6,7 @@
 /*   By: machaiba <machaiba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 12:32:35 by machaiba          #+#    #+#             */
-/*   Updated: 2023/05/25 16:11:10 by machaiba         ###   ########.fr       */
+/*   Updated: 2023/05/25 22:43:37 by machaiba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 int	filenames_check(char *str)
 {
 	if ((!(ft_strcmp(str, "<"))) || (!(ft_strcmp(str, ">")))
-		|| (!(ft_strcmp(str, "|"))) || (!(ft_strcmp(str, ";")))
+		|| (!(ft_strcmp(str, "<<"))) || (!(ft_strcmp(str, "<<")))
+		|| (!(ft_strncmp(str, "|", 1))) || (!(ft_strcmp(str, ";")))
 		|| (!(ft_strcmp(str, "#"))))
 		return (1);
 	return (0);
@@ -34,14 +35,14 @@ int	errors_check(t_token *lst)
 			write (2, "syntax error near unexpected token `newline'\n", 46);
 			return (1);
 		}
-		else if ((lst->next) && ((!(ft_strcmp(lst->data, "<"))) || (!(ft_strcmp(lst->data, "<<")))
+		if ((lst->next) && ((!(ft_strcmp(lst->data, "<"))) || (!(ft_strcmp(lst->data, "<<")))
 			|| (!(ft_strcmp(lst->data, ">")) || (!(ft_strcmp(lst->data, ">>"))))))
 		{
 			if (filenames_check(lst->next->data))
 			{
 				write (2, "syntax error near unexpected token `", 37);
-				// write (2, lst->next->data, 1);
-				ft_putstr_fd(lst->next->data, 2);
+				write (2, lst->next->data, 2);
+				// ft_putstr_fd(lst->next->data, 2);
 				write (2, "\'\n", 2);
 				return (1);
 			}
