cea7709692cce81d52bd23e7ecb58fcf1a91449f
Othmane Titebah <otitebah@e1r5p6.1337.ma>
2023-06-22 00:28:50 +0100

    fix some leaks

--- execution/builtins/export.c
+++ execution/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: otitebah <otitebah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 18:13:57 by otitebah          #+#    #+#             */
-/*   Updated: 2023/06/21 18:32:00 by otitebah         ###   ########.fr       */
+/*   Updated: 2023/06/22 00:17:58 by otitebah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,13 +69,17 @@ void    export_a(t_list **saving_env, t_list **saving_expo, t_args *p)
     i = 1;
     if (p->args[i])
     {
+                // system("leaks minishell");
         while (p->args[i])
         {
             if (error_export(p->args, i) == 0)
                 return ;
             else if (search_plus(p->args[i]) == 0 && search_egal(p->args[i]) == 0
                     && !search_node(*saving_expo, p->args[i]))
+            {
                 (*saving_expo) = export(p->args[i], &(*saving_expo));
+                // system("leaks minishell");
+            }
             else if (search_egal(p->args[i]) == 3)
             {
                 puts("hanu11111");
@@ -83,20 +87,19 @@ void    export_a(t_list **saving_env, t_list **saving_expo, t_args *p)
                 (*saving_expo) = export(p->args[i], &(*saving_expo));
             }
             else if (search_egal(p->args[i]) == 1 && search_plus(p->args[i]) == 0)
-            {
-
                 equal_no_plus(saving_expo, saving_env, p->args[i]);
-            }
+            //equal no plus mafihach leaks
             else if (search_plus(p->args[i]) == 2)
             {
                 puts("hanasssss");
-                    spl_p = ft_split(p->args[i], '+');
-                    if(if_plus(saving_expo, saving_env, p->args[i]) == 1);
-                    else if (search_node(*saving_expo, spl_p[0]))
-                    {
-                        if_plus2(saving_expo, saving_env, p->args[i]);
-                        if_plus3(saving_env, p->args[i], spl_p[0]);
-                    }
+                spl_p = ft_split(p->args[i], '+');
+                if(if_plus(saving_expo, saving_env, p->args[i]) == 1);
+                else if (search_node(*saving_expo, spl_p[0]))
+                {
+                    if_plus2(saving_expo, saving_env, p->args[i]);
+                    if_plus3(saving_env, p->args[i], spl_p[0]);
+                }
+                ft_free(spl_p);
             }
             i++;
         }
@@ -107,7 +110,6 @@ void    export_a(t_list **saving_env, t_list **saving_expo, t_args *p)
         if (*saving_expo)
             print_export(saving_expo);
         (*saving_expo) = tmp1;
-    // system("leaks minishell");
     }
 }
 
